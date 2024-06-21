42c9f46e80a290bbb4e655a5ea1577a7734d160f
KunalGosavi98 <kunalgosavi4298@gmail.com>
2024-06-21 13:26:19 +0530

    Initial commit

---
+++ src/main/java/com/SpringDemo/DemoController.java
@@ -0,0 +1,18 @@
+package com.SpringDemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+	
+	@GetMapping("/f1")
+	public String f1()
+	{
+		
+		return "Hello World Cicd PipeLine";
+	}
+
+}
